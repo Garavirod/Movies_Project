5c897432fa4038f943e72b98b890ea07be24a96b
Garavirod <rodrigogarciaavila26@gmail.com>
2020-01-13 21:55:44 -0600

    Agregar pelicula favorita

--- api_pelis/api/views.py
+++ api_pelis/api/views.py
@@ -1,7 +1,49 @@
-from .models import Pelicula
-from .serializers import PeliculaSerializer
-from rest_framework import viewsets
+from .models import Pelicula, PeliculaFavorita
+from .serializers import PeliculaSerializer, PeliculaFavoritaSerializer
+
+from django.shortcuts import get_object_or_404
+
+from rest_framework import viewsets, views
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 class PeliculaViewSet(viewsets.ModelViewSet):
   queryset = Pelicula.objects.all()
   serializer_class = PeliculaSerializer #This will transform to jason format
+
+class MarcarPeliculaFavorita(views.APIView): #This is a GenericAPI's view
+  # To marking and dismarking user must be authenticaed
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+  #Buscamor el obj con la película a partir del identificador 'id' que pasamos en los datos
+  # en ese diccionario 
+    def post(self, request):
+      pelicula = get_object_or_404(
+        Pelicula, id=self.request.data.get('id', 0) #diccionario
+      )
+
+      """
+        favorita : instacia creada
+        created: valor booleano (ya existe o no la pelicula)
+
+        si no existe , la marca como favorita si la encinta, existe 
+        y la desmarca como favorita
+      """
+      favorita, created = PeliculaFavorita.objects.get_or_create(
+        pelicula = pelicula, usuario=request.user
+      )
+
+        # Por defecto suponemos que se crea bien la instacia
+      content = {
+        'id': pelicula.id,
+        'favorita': True
+      }
+
+      # Si no se ha creado es que ya existe, entonces borramos el favorito
+      if not created:
+        favorita.delete()
+        content['favorita'] = False
+
+      return Response(content)
--- api_pelis/api_pelis/urls.py
+++ api_pelis/api_pelis/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from api import views
 from rest_framework import routers #Crea rutas automáticamente
 
@@ -11,6 +10,7 @@ router.register('peliculas', views.PeliculaViewSet)
 
 urlpatterns = [
   path('api/v1/', include(router.urls)),
+  path('api/v1/favorita/', views.MarcarPeliculaFavorita.as_view()),
   path('admin/', admin.site.urls),
   path('api/v1/auth/', include('rest_auth.urls')),
   path('api/v1/auth/registration/', include('rest_auth.registration.urls')),
